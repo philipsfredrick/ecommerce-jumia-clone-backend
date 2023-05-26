33b2d2820e0cfc325e32db39aecd5d80a08f2320
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 19:09:19 +0100

    added error codes file

---
+++ src/main/java/com/nonso/ecommercejumiaclone/payload/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.nonso.ecommercejumiaclone.payload;
+
+public enum ErrorCode {
+
+    EMAIL_ALREADY_IN_USE,
+
+    INVALID_CREDENTIALS,
+
+    INVALID_TOKEN,
+
+    MISSING_TOKEN,
+
+    CONSTRAINT_VIOLATION,
+
+    INSUFFICIENT_BALANCE,
+    WALLET_DOES_NOT_EXIST,
+    INVALID_INPUT_PROVIDED,
+
+    CURRENCY_CODE_MISMATCH,
+
+    TOKEN_EXPIRED
+}
