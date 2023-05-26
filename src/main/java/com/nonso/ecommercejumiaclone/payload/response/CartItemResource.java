3432456477c45ac01b4d66901cde6e4e3867d344
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 18:58:26 +0100

    added cart-item resource

---
+++ src/main/java/com/nonso/ecommercejumiaclone/payload/response/CartItemResource.java
@@ -0,0 +1,25 @@
+package com.nonso.ecommercejumiaclone.payload.response;
+
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemResource implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -3610419978750672391L;
+
+    private Long id;
+    private String productName;
+    private String imageUrl;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
