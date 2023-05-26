2580296630d52713580a1040e8e153b40e2933cf
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 19:10:05 +0100

    added spring boot application main file

--- src/main/java/com/nonso/ecommercejumiaclone/EcommerceJumiaCloneApplication.java
+++ src/main/java/com/nonso/ecommercejumiaclone/EcommerceJumiaCloneApplication.java
@@ -1,19 +1,10 @@
 package com.nonso.ecommercejumiaclone;
 
-import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
-import com.nonso.ecommercejumiaclone.service.CartService;
-import com.nonso.ecommercejumiaclone.utils.CloudinaryService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -22,5 +13,4 @@ public class EcommerceJumiaCloneApplication {
     public static void main(String[] args) throws IOException {
         SpringApplication.run(EcommerceJumiaCloneApplication.class, args);
     }
-
 }
