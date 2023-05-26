c0b3855b4d3fd67b5e2fc2364955c3b22d21f46d
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 18:59:45 +0100

    added cart repository

--- src/main/java/com/nonso/ecommercejumiaclone/repository/CartRepository.java
+++ src/main/java/com/nonso/ecommercejumiaclone/repository/CartRepository.java
@@ -2,15 +2,20 @@ package com.nonso.ecommercejumiaclone.repository;
 
 import com.nonso.ecommercejumiaclone.entities.Cart;
 import com.nonso.ecommercejumiaclone.entities.User;
+import com.nonso.ecommercejumiaclone.entities.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     List<Cart> findAllByUser(User user);
+
+    Optional<Cart> findCartByUser_EmailAndUser_Role(String email, UserRole userRole);
+    Optional<Cart> findByUserId(Long userId);
 
     List<Cart> findAllByUserOrderByCreatedAtDesc(User user);
 }
