9585a9fb08c5feb42527740e9946b549e5bb1ab2
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 19:01:24 +0100

    implemented cart service

--- src/main/java/com/nonso/ecommercejumiaclone/service/CartService.java
+++ src/main/java/com/nonso/ecommercejumiaclone/service/CartService.java
@@ -1,13 +1,20 @@
 package com.nonso.ecommercejumiaclone.service;
 
 import com.nonso.ecommercejumiaclone.entities.Cart;
-import com.nonso.ecommercejumiaclone.payload.request.AddToCartDto;
-import com.nonso.ecommercejumiaclone.payload.response.ApiResponse;
+import com.nonso.ecommercejumiaclone.payload.response.CartItemResource;
+import com.nonso.ecommercejumiaclone.payload.response.CartResource;
 
 import java.util.List;
 
-public interface CartService {
-    ApiResponse<Cart> addToCart(AddToCartDto addToCartDto) throws Exception;
 
-    ApiResponse<List<Cart>> viewCart() throws Exception;
+public interface CartService {
+
+    Cart createCart();
+    CartResource addItemToCart(Long productId, Integer quantity);
+
+    CartResource getCartByUserId(Long userId);
+
+    CartResource removeItemFromCart(Long cartId, Long cartItemId);
+
+    List<CartItemResource> getAllCartItems(Long cartId);
 }
