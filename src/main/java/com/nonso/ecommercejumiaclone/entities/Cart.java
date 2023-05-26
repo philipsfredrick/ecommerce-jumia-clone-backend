280e6f08737a233990403868251c424e7c70199a
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 18:31:07 +0100

    cart entity

--- src/main/java/com/nonso/ecommercejumiaclone/entities/Cart.java
+++ src/main/java/com/nonso/ecommercejumiaclone/entities/Cart.java
@@ -1,41 +1,60 @@
 package com.nonso.ecommercejumiaclone.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@Setter
 @Entity
-@Table(name = "cart")
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE carts SET deleted_at = NOW() WHERE id = ?", check = ResultCheckStyle.COUNT)
+@Where(clause = "deleted_at IS NULL")
+@Table(name = "carts")
 public class Cart {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
+    @NotNull(message = "Missing required field grandTotal")
+    @Column(name = "grand_total", nullable = false)
+    private BigDecimal grandTotal;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonBackReference
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<CartItem> cartItems = new ArrayList<>();
+
     @CreationTimestamp
+    @Setter(AccessLevel.NONE)
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Setter(AccessLevel.NONE)
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    User user;
-
-    @Column(name = "quantity")
-    private int quantity;
+    @Setter(AccessLevel.NONE)
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
