0f188b8c59fc26ff0d055932cc89ea81b657692a
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 18:57:54 +0100

    added cart-item repository

---
+++ src/main/java/com/nonso/ecommercejumiaclone/repository/CartItemRepository.java
@@ -0,0 +1,11 @@
+package com.nonso.ecommercejumiaclone.repository;
+
+import com.nonso.ecommercejumiaclone.entities.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    List<CartItem> findCartItemsByCart_IdAndOrderIsNull(Long cartId);
+}
