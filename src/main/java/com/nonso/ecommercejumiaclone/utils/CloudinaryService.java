4bf8f0910ecdb7d075a9930545c13738d14c3092
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 19:06:17 +0100

    added cloudinary service for image upload

--- src/main/java/com/nonso/ecommercejumiaclone/utils/CloudinaryService.java
+++ src/main/java/com/nonso/ecommercejumiaclone/utils/CloudinaryService.java
@@ -12,9 +12,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
-
-@Component
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class CloudinaryService {
 
