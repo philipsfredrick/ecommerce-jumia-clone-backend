862a25b108f9c76279d5507c860cd30373fb95a6
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 19:19:11 +0100

    added unauthorized exception

---
+++ src/main/java/com/nonso/ecommercejumiaclone/exception/UnAuthorizedException.java
@@ -0,0 +1,21 @@
+package com.nonso.ecommercejumiaclone.exception;
+
+import com.nonso.ecommercejumiaclone.payload.ErrorCode;
+
+import java.io.Serial;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+public class UnAuthorizedException extends JumiaCloneException{
+
+    @Serial
+    private static final long serialVersionUID = -321597052401234702L;
+
+    public UnAuthorizedException(String message) {
+        super(message, UNAUTHORIZED);
+    }
+
+    public UnAuthorizedException(String message, ErrorCode errorCode) {
+        super(message, UNAUTHORIZED, errorCode);
+    }
+}
