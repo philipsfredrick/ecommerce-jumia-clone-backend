522040baf74b82d5d7ca27edf67abaffa27b7a89
philipsfredrick <philipsfredrick@yahoo.com>
2023-05-26 18:24:43 +0100

    api error codes for exceptions

---
+++ src/main/java/com/nonso/ecommercejumiaclone/exception/handler/ApiError.java
@@ -0,0 +1,73 @@
+package com.nonso.ecommercejumiaclone.exception.handler;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nonso.ecommercejumiaclone.payload.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiError {
+
+    @JsonIgnore
+    private int status;
+
+    private String message;
+
+    private String path;
+
+    private String errorCode;
+
+    private String infoLink;
+
+    private Object details;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timeStamp;
+
+    public ApiError(final int status, final String message, final String path, final ErrorCode errorCode) {
+        timeStamp = LocalDateTime.now();
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.errorCode = (errorCode != null) ? errorCode.toString() : null;
+    }
+
+
+    public ApiError(final int status, final String message, final String path, final String errorCode) {
+        timeStamp = LocalDateTime.now();
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.errorCode = errorCode;
+    }
+
+
+    public ApiError(final int status, final String message, final String path) {
+        timeStamp = LocalDateTime.now();
+        this.status = status;
+        this.message = message;
+        this.path = path;
+    }
+
+
+    public ApiError(final String message, final Object details) {
+        timeStamp = LocalDateTime.now();
+        this.message = message;
+        this.details = details;
+    }
+
+    public ApiError(final String message, final Object details, final String errorCode) {
+        timeStamp = LocalDateTime.now();
+        this.message = message;
+        this.details = details;
+        this.errorCode = errorCode;
+    }
+}
