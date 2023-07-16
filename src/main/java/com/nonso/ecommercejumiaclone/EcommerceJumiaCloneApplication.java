d680d5109ed67cac247f43fdae2ece32c7d9dde2
philipsfredrick <philipsfredrick@yahoo.com>
2023-07-17 00:26:06 +0100

    removed command line runner from main method class

--- src/main/java/com/nonso/ecommercejumiaclone/EcommerceJumiaCloneApplication.java
+++ src/main/java/com/nonso/ecommercejumiaclone/EcommerceJumiaCloneApplication.java
@@ -1,8 +1,11 @@
 package com.nonso.ecommercejumiaclone;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
 
